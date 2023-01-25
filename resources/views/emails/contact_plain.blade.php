67ffb18efe230db450f60c124e4a80a1d6b3a784
Albin Nilsson <albin@myrmalm.se>
2023-01-25 14:55:48 +0100

    add line break

--- resources/views/emails/contact_plain.blade.php
+++ resources/views/emails/contact_plain.blade.php
@@ -1,11 +1,13 @@
-Från: {{ $contactData['name']}} ({{ $contactData['email'] }})
-@if(isset($contactData['phone']))
+Från: {{ $contactData['name'] }} ({{ $contactData['email'] }})
+@if (isset($contactData['phone']))
 Tel: {{ $contactData['phone'] }}
 @endif
 
 Meddelande:
 ====
+
 {{ $contactData['message'] }}
+
 ====
 
 Tidpunkt: {{ now()->timezone('Europe/Stockholm') }}
